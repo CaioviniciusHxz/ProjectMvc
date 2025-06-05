e1f8debfee08eda987aa33b87f5c36a307ade836
Caio <caioviniciusdev21@gmail.com>
2025-06-04 22:17:37 -0300

    Primeiro crud com migration

---
+++ ProjectMvc/Data/ProjectMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectMvc.Models;
+
+namespace ProjectMvc.Data
+{
+    public class ProjectMvcContext : DbContext
+    {
+        public ProjectMvcContext (DbContextOptions<ProjectMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectMvc.Models.Departament> Departament { get; set; } = default!;
+    }
+}
